390fb395c3ae2bd4e009ff1eea02128263595b25
Ianzb <93322252@qq.com>
2023-09-28 20:32:28 +0800

    2.1.2

--- Windows/moddownloader.py
+++ Windows/moddownloader.py
@@ -1,7 +1,16 @@
-version = "0.0.4"
-import requests, json
+version = "0.0.5"
+import requests, json, os, hashlib
 
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.36"}
+
+
+# 路径拼接
+def join(*data):
+    path = ""
+    for i in data:
+        path = os.path.join(path, i)
+    path = path.replace("//", r"\ "[:-1]).replace(r"\\ "[:-1], r"\ "[:-1]).replace("\/", r"\ "[:-1]).replace("/\ "[:-1], r"\ "[:-1]).replace("/", r"\ "[:-1])
+    return path
 
 
 # str转json
@@ -95,7 +104,7 @@ def searchModInf(data):
 def getModData(data: list):
     for i in range(len(data)):
         data[i] = data[i]["ID"]
-    response = requests.get(joinUrl("https://api.modrinth.com/v2/projects", {"ids": data}), headers=header, stream=True).text
+    response = requests.get(joinUrl("https://api.modrinth.com/v2/projects", {"ids": data}), headers=header, timeout=600).text
     response = load(response)
     list = []
     for i in response:
@@ -119,7 +128,7 @@ def getModVersions(data: dict):
     list = []
     for i in data["模组版本"]:
         list.append(i)
-    response = requests.get(joinUrl("https://api.modrinth.com/v2/versions", {"ids": list}), headers=header, stream=True).text
+    response = requests.get(joinUrl("https://api.modrinth.com/v2/versions", {"ids": list}), headers=header, timeout=600).text
     response = load(response)
     list = modVersionsInf(response)
     return list
@@ -130,7 +139,7 @@ def getShaVersions(data: list):
     list = []
     for i in data:
         try:
-            response = requests.get("https://api.modrinth.com/v2/version_file/" + i, headers=header, stream=True).text
+            response = requests.get("https://api.modrinth.com/v2/version_file/" + i, headers=header, timeout=600).text
             response = load(response)
             list.append(response)
         except:
@@ -156,25 +165,63 @@ def sortVersionsFiles(data: list):
 
 # 获取前置模组
 def getModDepends(data):
-    response = requests.get("https://api.modrinth.com/v2/project/" + data["ID"] + "/dependencies", headers=header, stream=True).text
+    response = requests.get("https://api.modrinth.com/v2/project/" + data["ID"] + "/dependencies", headers=header, timeout=600).text
     response = load(response)
     list = response["projects"]
     list = modVersionsInf(list)
     return list
 
 
+# 获取mc版本列表
+def getGameVersions():
+    list = []
+    response = requests.get("https://api.modrinth.com/v2/tag/game_version", headers=header, timeout=600).text
+    response = load(response)
+    for i in response:
+        if i["version_type"] == "release":
+            list.append(i["version"])
+    return list
+
+
+# 获取文件夹里面所有文件的sha1，格式{"文件名":"sha1值"}
+def getShaList(data):
+    dict = {}
+    list = []
+    list2 = []
+    for i in os.walk(data):
+        list.append(i)
+    list = list[0][2]
+    list = [join(data, i) for i in list]
+    for i in range(len(list)):
+        with open(list[i], "rb") as file:
+            list2.append(hashlib.sha1(file.read()).hexdigest())
+    for i in range(len(list)):
+        dict[list[i]] = list2[i]
+
+    return dict
+
+
 # 检查sha1对应模组有无更新
-def checkShaUpdate(data: list, needVersion: str, needLoader: str):
-    for i in range(len(data)):
-        print("开始", data[i], "检查更新")
-        list = getShaVersions([data[i]])
+def checkShaUpdate(data, needVersion: str, needLoader: str):
+    needLoader.lower()
+    data = getShaList(data)
+    export = []
+    for k, i in data.items():
+        print("开始检查更新", k)
+        list = getShaVersions([i])
         if not list:
-            print("未找到模组", data[i])
+            print("未找到模组", k)
             continue
-        print("找到文件信息了", list[0])
+        print("找到文件信息", list[0]["名称"])
         list = getModData(list)
+        if not list:
+            print("这个文件对应的模组无了")
+            continue
         print("找到对应模组了", list[0]["名称"])
         list = getModVersions(list[0])
+        if not list:
+            print("这个模组的文件无了")
+            continue
         print("正在翻找模组版本")
         list = sortVersionsFiles(list)
         if needVersion not in list:
@@ -185,21 +232,40 @@ def checkShaUpdate(data: list, needVersion: str, needLoader: str):
         for j in list[::-1]:
             if needLoader in j["加载器"]:
                 list2.append(j)
-        print(list2)
         if not list2:
             print("模组更新了", needVersion, "但没有更新", needLoader, "加载器的这个版本")
             continue
         list2 = list2[0]
-        if list2["sha1"] == data[i]:
-            print("当前模组已经是最新的了", list2)
-        elif list2["sha1"] != data[i]:
-            print("当前模组有新版本", list2)
+        if list2["sha1"] == i:
+            print("当前模组已经是最新的了")
+        elif list2["sha1"] != i:
+            print("当前模组有新版本", list2["文件名"])
+            export.append({"路径": os.path.dirname(k), "旧名称": os.path.basename(k), "新名称": list2["文件名"], "链接": list2["下载链接"]})
+    return export
 
 
-checkShaUpdate(["2eed0709f11a57720f57243345275703305746a2"], "1.20.2", "fabric")
+# 承接上面直接更新模组
+def modUpdate(data):
+    for i in data:
+        try:
+            response = requests.get(i["链接"], headers=header, timeout=600).content
+            with open(join(i["路径"], i["新名称"]), "wb") as file:
+                file.write(response)
+            os.rename(join(i["路径"], i["旧名称"]), join(i["路径"], i["旧名称"]) + ".old")
+            print(f"{i['新名称']}下载成功")
+        except:
+            print(f"{i['新名称']}下载失败")
+
+
+data = checkShaUpdate(r"D:\Minecraft\Minecraft Java Edition\.minecraft\versions\CM外挂端1.20.1-2.0.0\mods", "1.20.1", "fabric")
+print(data)
+if input("是否更新？") == "1":
+    modUpdate(data)
+
 '''
 2023年9月24日：0.0.1：添加搜索api接入
 2023年9月25日：0.0.2：完善搜索功能，丰富可操控参数，修复Bug，添加简易的搜索使用
 2023年9月26日：0.0.3：添加获取模组页面信息，获取版本列表，sha1对应版本
 2023年9月27日：0.0.4：添加模组版本按照游戏版本整理并提供链接功能，添加获取前置模组，简易版检查模组更新功能
+2023年9月28日：0.0.5：完美完成检查指定文件夹模组更新的功能，添加获取mc正式版列表功能
 '''
