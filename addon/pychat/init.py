c528e97521834fec399fbc806674fd2d522f72a6
Ianzb <93322252@qq.com>
2023-12-07 22:18:04 +0800

    3.3.0

--- addon/pychat/init.py
+++ addon/pychat/init.py
@@ -5,7 +5,7 @@ try:
 except:
     sys.path = [os.path.dirname(sys.argv[0])] + sys.path
     from source.custom import *
-os.chdir(__file__)
+os.chdir(os.path.dirname(__file__))
 from chatapi import *
 
 
