8084f60107d956ef1679f00cc9ff3b6bedd73717
Ianzb <93322252@qq.com>
2023-12-19 19:40:34 +0800

    beta 3.5.0

--- release/source/init.py
+++ release/source/init.py
@@ -24,6 +24,7 @@ class Init():
         # 重复运行检测
         if "python" in f.cmd(f"tasklist |findstr {setting.read("pid")}", True):
             setting.save("showWindow", "1")
+            logging.info(f"当前程序为重复运行，自动退出")
             sys.exit()
         setting.save("pid", str(program.PROGRAM_PID))
 
