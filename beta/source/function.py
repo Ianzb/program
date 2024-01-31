ac0a4576ed120b41dda44fdc8dc9f663a7d47b67
Ianzb <93322252@qq.com>
2024-01-31 16:35:18 +0800

    3.5.2

--- beta/source/function.py
+++ beta/source/function.py
@@ -23,7 +23,7 @@ class Program:
     程序信息
     """
     PROGRAM_NAME = "zb小程序"  # 程序名称
-    PROGRAM_VERSION = "3.5.1"  # 程序版本
+    PROGRAM_VERSION = "3.5.2"  # 程序版本
     PROGRAM_TITLE = f"{PROGRAM_NAME} {PROGRAM_VERSION}"  # 程序窗口标题
     AUTHOR_NAME = "Ianzb"  # 作者名称
     AUTHOR_URL = "https://ianzb.github.io/"  # 作者网址
