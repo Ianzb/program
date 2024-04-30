50bbccd9cd0d4ed1c17cd0f040f57ab0ea7c5e39
Ianzb <93322252@qq.com>
2024-04-30 20:19:27 +0800

    脚本更新

--- pytoexe.bat
+++ pytoexe.bat
@@ -1,3 +1,3 @@
 chcp 65001
-rmdir "D:\编程\打包"
+rmdir /S /Q "D:\编程\打包"
 pyinstaller -D -w "D:\编程\program\program\main.pyw" -i "D:\编程\program\program\source\img\program.ico" -n zbProgram --distpath "D:\编程\打包" --workpath "D:\编程\打包\build" --clean --contents-directory source --add-data D:\编程\program\program\source\img:img -y
