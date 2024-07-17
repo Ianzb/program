9aaa7fb22e246df838e48b2b0ee7f89556af93c2
Ianzb <93322252@qq.com>
2024-07-17 17:51:06 +0800

    脚本更新

--- packaging/exe.bat
+++ packaging/exe.bat
@@ -4,3 +4,4 @@ call D:\Code\program\venv\Scripts\activate.bat
 pyinstaller -D -w "D:\Code\program\program\main.pyw" -i "D:\Code\program\program\source\img\program.ico" -n zbProgram --distpath "D:\Code\打包" --workpath "D:\Code\打包\build" --clean --contents-directory source --add-data D:\Code\program\program\source\img:img -y
 call D:\Code\program\venv\Scripts\deactivate.bat
 start setup.iss
+pause
